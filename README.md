11a34c0c5507685225560df293d99d1e7b5aada0
Nipun Balan Thekkummal <nipunbalan@gmail.com>
2019-08-01 02:20:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# LEWS Twitter Data Collector
+
+This program collects Twitter data in real-time and publishes to a Kafka broker
+
+## Pre-Requisites
+
+- Docker 18.09.0 or higher
+
+- Kafka Brocker
+
+- Twitter API Credentials (access token, access token secret, consumer key, consumer secret)
+
+
+## Building the Docker Image
+
+
+```bash
+docker build --tag lews-twitter-collector .
+```
+
+## Usage
+
+```bash
+docker run -e TWITTER_ACCESS_TOKEN="<access token>" \
+-e TWITTER_ACCESS_TOKEN_SECRET="<access token secret>" \
+-e TWITTER_CONSUMER_KEY="<consumer key>" \
+-e TWITTER_CONSUMER_SECRET="<consumer secret>" \
+-e KAFKA_BROKER="<kafka-broker-host:port>" lews-twitter-collector
+```
---
+++ lews_tweet_stream.py
@@ -0,0 +1,32 @@
+import os
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from kafka import SimpleProducer, KafkaClient
+
+# Print Twitter credentials -- Testing
+#print(os.getenv('TWITTER_ACCESS_TOKEN', 'NA'))
+#print(os.getenv('TWITTER_ACCESS_TOKEN_SECRET', 'NA'))
+#print(os.getenv('TWITTER_CONSUMER_KEY', 'NA'))
+#print(os.getenv('TWITTER_CONSUMER_SECRET', 'NA'))
+
+access_token =  os.getenv('TWITTER_ACCESS_TOKEN', 'NA')
+access_token_secret =  os.getenv('TWITTER_ACCESS_TOKEN_SECRET','NA')
+consumer_key =  os.getenv('TWITTER_CONSUMER_KEY','NA')
+consumer_secret =  os.getenv('TWITTER_CONSUMER_SECRET','NA')
+
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        producer.send_messages(os.getenv('KAFKA_TOPIC','lews-twitter'), data.encode('utf-8'))
+        print (data)
+        return True
+    def on_error(self, status):
+        print (status)
+
+kafka = KafkaClient(os.getenv('KAFKA_BROKER','host.docker.internal:9092'))
+producer = SimpleProducer(kafka)
+l = StdOutListener()
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+stream = Stream(auth, l)
+stream.filter(track="rain")
